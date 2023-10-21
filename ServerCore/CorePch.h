ea2f3b3cef8c3f92be23c885dc935ae10b702e40
Beom Jun Kang <nature4724@gmail.com>
2023-10-21 18:02:09 +0900

    헤더 정리  Server base

--- ServerCore/CorePch.h
+++ ServerCore/CorePch.h
@@ -1,18 +1,51 @@
 #pragma once
+//----------
+//	Window, STL
+//----------
+#include <iostream>
+#include <atomic>
+#include <mutex>
+#include <memory>
+#include <future>
+#include <Windows.h>
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include<WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
 
+//-----------------
+//Global Type Macro
+//-----------------
 #include "Types.h"
 #include "CoreMacro.h"
 #include "CoreGlobal.h"
-#include "CoreTLS.h"
+
+//----------
+//	Memory
+//----------
+#include "Allocator.h"
+#include "Memory.h"
 #include "Container.h"
-
-
-
-#include <iostream>
-#include <Windows.h>
-
-#include "Lock.h"
 #include "ObjectPool.h"
-using namespace std;
+
+//----------
+//	Lock
+//----------
+#include "Lock.h"
+
+//----------
+//	Thread
+//----------
+#include "ThreadManager.h"
+#include "CoreTLS.h"
+
+
+//----------
+//	Smart Pointer
+//----------
+#include "RefCounting.h"
+
+
 
 
