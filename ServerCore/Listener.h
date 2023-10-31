aef079a58c07d0491bd7e7c707fc5b26245901ef
Beom Jun Kang <nature4724@gmail.com>
2023-11-01 00:38:23 +0900

    5

--- ServerCore/Listener.h
+++ ServerCore/Listener.h
@@ -1,26 +1,38 @@
 #pragma once
-
 #include "IocpCore.h"
-#include "IocpEvent.h"
+#include "NetAddress.h"
 
 class AcceptEvent;
+class ServerService;
+
+/*--------------
+	Listener
+---------------*/
 
 class Listener : public IocpObject
 {
 public:
-    Listener() = default;
-    ~Listener();
-public:
-    bool StartAccept(NetAddress netAddress);
-    void CloseSocket();
+	Listener() = default;
+	~Listener();
 
-    virtual HANDLE GetHandle() override;
-    virtual void Dispatch(class IocpEvent* iocpEvent, DWORD numOfBytes = 0) override;
+public:
+	/* 외부에서 사용 */
+	bool StartAccept(ServerServiceRef service);
+	void CloseSocket();
+
+public:
+	/* 인터페이스 구현 */
+	virtual HANDLE GetHandle() override;
+	virtual void Dispatch(class IocpEvent* iocpEvent, int32 numOfBytes = 0) override;
 
 private:
-    void RegisterAccept(AcceptEvent* acceptEvent);
-    void ProcessAccept(AcceptEvent* acceptEvent);
+	/* 수신 관련 */
+	void RegisterAccept(AcceptEvent* acceptEvent);
+	void ProcessAccept(AcceptEvent* acceptEvent);
+
 protected:
-    SOCKET _socket = INVALID_SOCKET;
-    Vector<AcceptEvent*> _acceptEvent;
+	SOCKET _socket = INVALID_SOCKET;
+	Vector<AcceptEvent*> _acceptEvents;
+	ServerServiceRef _service;
 };
+
