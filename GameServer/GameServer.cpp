a83eb40c35c37d57c3286d3a285ac808c36345fd
Beom Jun Kang <nature4724@gmail.com>
2023-11-21 00:08:56 +0900

    job큐 버전1

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,17 +4,27 @@
 #include "GameSessionManager.h"
 #include "Protocol.pb.h"
 #include "ClientPacketHandler.h"
+#include "Job.h"
+#include "Room.h"
 
 
 int main()
 {
-	ClientPacketHandler::Init();
+	{
+		HealJob healJob;
+		healJob._target = 1;
+		healJob._healValue = 10;
+
+
+		healJob.Excute();
+	}
 	ServerServiceRef service = MakeShared<ServerService>(
 	NetAddress(L"127.0.0.1", 7777),
 	MakeShared<IocpCore>(),
 	MakeShared<GameSession>,
 	100
 	);
+	ClientPacketHandler::Init();
 
 	ASSERT_CRASH(service->Start());
 	
@@ -29,8 +39,13 @@ int main()
 			}
 		});
 	}
-
+	while(true)
+	{
+		GRoom.FlushJob();
+		this_thread::sleep_for(1ms);
+	}
 	GThreadManager->Join();
+
 }
 
 
